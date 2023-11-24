f5f5887948a8ec24f781a3810bcd2e2c25138365
ExcaCambo <bongchannarith@gmail.com>
2023-11-24 14:11:32 +0700

    add

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 VERSION=${1:-'1.0.0'}
-
+appName='react-admin-web'
 composeFile='docker-compose.yaml'
-sed -i "s|/react-admin-web:latest|/react-admin-web:${VERSION}|" ${composeFile}
-# docker rm -f react-admin-web
+sed -i "s|/react-admin-web:latest|/${appName}:${VERSION}|" ${composeFile}
+
+containerId=$(docker ps --all | grep "${appName}" | awk {'print $1'})
+if [ "$containerId" ]; then
+  docker rm -f $containerId
+fi
 docker compose -f ${composeFile} up -d --force-recreate
